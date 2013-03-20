b57a159e602cf0ca1a42a124296a3ba7e3d2e31a
Sandro Padin <sandropadin@gmail.com>
2013-03-20 11:45:29 -0500

    Refactoring spec

--- spec/ticlj_on_jerver/controller/server_spec.clj
+++ spec/ticlj_on_jerver/controller/server_spec.clj
@@ -3,24 +3,26 @@
         [ticlj-on-jerver.controller.server]))
 
 (describe "ticlj-on-jerver.controller.server"
+  (with sample-server (reify com.jerver.http.server.Server
+                         (setPublicDirectory [_ directory]
+                           (println directory))
+                         (run [_]
+                           (println "run"))))
+
   (it "calls ServerImpl#setPublicDirectory"
-    (let [server (reify com.jerver.http.server.Server
-                    (setPublicDirectory [_ directory]
-                      (print directory)))]
-      (should= "test-directory" (with-out-str (-> server (set-public-directory "test-directory"))))))
+    (let [server @sample-server]
+      (should-contain "test-directory"
+                      (with-out-str (-> server
+                                        (set-public-directory "test-directory"))))))
 
   (it "calls ServerImpl#run"
-    (let [server (reify com.jerver.http.server.Server
-                    (run [_]
-                      (print "stubbed-run")))]
-      (should= "stubbed-run" (with-out-str (run server)))))
+    (let [server @sample-server]
+      (should-contain "run"
+                      (with-out-str (run server)))))
 
   (it "calls ServerImpl#setPublicDirectory then ServerImpl#run"
-    (let [server (reify com.jerver.http.server.Server
-                    (setPublicDirectory [_ directory]
-                      (println "setPublicDirectory"))
-                    (run [_]
-                      (println "run")))]
-      (should= "setPublicDirectory\nrun\n" (with-out-str (-> server
-                                               (set-public-directory "test-directory")
-                                               (run)))))))
+    (let [server @sample-server]
+      (should= "test-directory\nrun\n"
+               (with-out-str (-> server
+                                 (set-public-directory "test-directory")
+                                 (run)))))))
